6250a5121ac9f7e6f17555e39c2b980d00687926
Sergey Korsakov <korsakov.serg@gmail.com>
2023-12-19 19:36:04 +0100

    * Added more configuration settings to the configuration page

--- thankyou/slackbot/handlers/configuration.py
+++ thankyou/slackbot/handlers/configuration.py
@@ -1,7 +1,5 @@
-from slack_sdk import WebClient
-
+from thankyou.dao import dao
 from thankyou.slackbot.app import app
-from thankyou.slackbot.handlers.common import get_user_info
 from thankyou.slackbot.utils.company import get_or_create_company_by_body
 from thankyou.slackbot.views.configuration import configuration_view
 
@@ -9,10 +7,9 @@ from thankyou.slackbot.views.configuration import configuration_view
 def home_page_configuration_button_clicked_action_handler(body, logger):
     logger.info(body)
     user_id = body["user"]["id"]
-    user_info = get_user_info(user_id)
     company = get_or_create_company_by_body(body)
 
     app.client.views_publish(
         user_id=user_id,
-        view=configuration_view()
+        view=configuration_view(thank_you_types=dao.read_thank_you_types(company_uuid=company.uuid))
     )
--- thankyou/slackbot/views/configuration.py
+++ thankyou/slackbot/views/configuration.py
@@ -1,10 +1,14 @@
-from slack_sdk.models.blocks import SectionBlock, ButtonElement, HeaderBlock, ContextBlock, TextObject
+from typing import List
+
+from slack_sdk.models.blocks import SectionBlock, ButtonElement, HeaderBlock, ContextBlock, TextObject, \
+    ChannelSelectElement, StaticSelectElement, Option, UserMultiSelectElement, ActionsBlock
 from slack_sdk.models.views import View
 
+from thankyou.core.models import ThankYouType
 from thankyou.slackbot.blocks.homepage import home_page_actions_block
 
 
-def configuration_view():
+def configuration_view(thank_you_types: List[ThankYouType]):
     return View(
         type="home",
         title="Configuration",
@@ -22,11 +26,48 @@ def configuration_view():
                 text="Access to the configuration page"
             ),
             SectionBlock(
-                text="Admins: ",
-                accessory=ButtonElement(
-                    text="Edit...",
-                    action_id="configuration_view_edit_admins_button_clicked"
-                )
-            )
+                text="Select users that will have an access to this Configuration page",
+                accessory=UserMultiSelectElement()
+            ),
+            HeaderBlock(
+                text="Notification Settings"
+            ),
+            SectionBlock(
+                text="Share all thank yous in a Slack channel",
+                accessory=ChannelSelectElement()
+            ),
+            HeaderBlock(
+                text="Leaderboards Settings"
+            ),
+            SectionBlock(
+                text="Time period to use",
+                accessory=StaticSelectElement(options=[
+                    Option(value="last_30_days", label="Last 30 days"),
+                    Option(value="last_full_month", label="Last full month"),
+                    Option(value="last_7_days", label="Last 7 days"),
+                    Option(value="last_full_week", label="Last full week"),
+                ])
+            ),
+            HeaderBlock(
+                text="Weekly limits"
+            ),
+            SectionBlock(
+                text="How many thank yous a user can send in one week",
+                accessory=StaticSelectElement(options=[
+                    Option(value=str(num), label=str(num)) for num in range(1, 6)
+                ])
+            ),
+            HeaderBlock(
+                text="Company values"
+            ),
+            *[
+                SectionBlock(
+                    text=f"*{thank_you_type.name}*",
+                    accessory=ButtonElement(text="Edit..."),
+                ) for thank_you_type in thank_you_types
+            ],
+            ActionsBlock(
+                elements=[ButtonElement(text="Add a new value...")]
+            ),
         ]
     )
